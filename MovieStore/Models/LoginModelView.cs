ccc50533c711849967a849780f1e999ebb5edd62
whitedadam <whitedadam@yahoo.com>
2020-11-17 14:23:53 -0500

    Added Login Functionality
    - Added LoginViewModel to Models
    - Added Login View to View/Home

---
+++ MovieStore/Models/LoginModelView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieStore.Models
+{
+    public class LoginModelView
+    {
+        public string UserUserName { get; set; }
+        public string UserPassword { get; set; }
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
