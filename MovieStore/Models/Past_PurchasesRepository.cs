44a528d02913f61f12ec6ea87c74e7d0dd492ab6
kattypo <74074509+kattypo@users.noreply.github.com>
2020-11-18 13:26:50 -0500

    Update Past_PurchasesRepository.cs

--- MovieStore/Models/Past_PurchasesRepository.cs
+++ MovieStore/Models/Past_PurchasesRepository.cs
@@ -14,5 +14,18 @@ namespace MovieStore.Models
             _context = context;
         }
 
+        public Past_Purchases GetUserTransactions(Past_Purchases past_PurchaseIn)
+
+        {
+            IEnumerable<Transaction> userTransactions = _context.Transactions.Where(t => t.Customer == past_PurchaseIn.Customer);
+
+             String GetMovieName(IEnumerable<Transaction>)
+            {
+   
+                return Transaction.Movie(t => t.Movie == Movie).Name;
+            }
+
+            return past_PurchaseIn;
+        }
     }
 }
